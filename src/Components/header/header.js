6b6670e09d960660d9d8ef9183aeb81a8c9c79f7
Robin Khiv <robinkhiv@gmail.com>
2019-06-17 22:47:15 -0700

    added nav links

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react'
 export default class header extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <header>
+                <nav>
+                    <div>
+                        <a>HOME</a>
+                    </div>
+                    <ul>
+                        <li>
+                            TRAVEL
+                        </li>
+                        <li>
+                            EAT
+                        </li>
+                        <li>
+                            ATTEND
+                        </li>
+                        <li>
+                            ABOUT ME
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
